14aecc7434f41564b5795a5224389a441a4ac806
Yaroslav Roschupkin <yaroslav-r.01@mail.ru>
2020-09-15 18:16:47 +0300

    modified mistake check, simplified output

--- src/ru/vsu/cs/roschupkin_ya_a/Main.java
+++ src/ru/vsu/cs/roschupkin_ya_a/Main.java
@@ -1,36 +1,37 @@
 package ru.vsu.cs.roschupkin_ya_a;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        Locale.setDefault(Locale.ROOT);
+
         Scanner sc = new Scanner(System.in);
         //entering radii of circles, where r1 is the smallest
 
         System.out.print("enter the radius of the smallest circle R = ");
-        double r1 = sc.nextFloat();
+        double r1 = sc.nextDouble();
 
         System.out.print("enter the radius of the medium circle   R = ");
-        double r2 = sc.nextFloat();
+        double r2 = sc.nextDouble();
 
         System.out.print("enter the radius of the biggest circle  R = ");
-        double r3 = sc.nextFloat();
+        double r3 = sc.nextDouble();
 
         System.out.println();
 
-        if (!(anyMistakes(r1, r2, r3)))
+        if (!(checkMistakes(r1, r2, r3)))
         {
-            System.out.print("filled area is ");
-            System.out.println(countArea(r1, r2, r3));
+            System.out.printf("filled area is %.3f", calcArea(r1, r2, r3));
         }
-
     }
 
-    public static boolean anyMistakes (double r1, double r2, double r3) //checking r3 > r2 > r1
+    public static boolean checkMistakes (double r1, double r2, double r3) //checking r3 > r2 > r1 > 0
     {
-        if ((r1 >= r2) || (r1 >= r3) || (r2 >= r3))
+        if (((r1 >= r2) || (r1 >= r3) || (r2 >= r3)) || (r1 < 0))
         {
             System.out.println("something is wrong");
             return true;
@@ -38,7 +39,7 @@ public class Main
         else return false;
     }
 
-    public static double countArea(double r1, double r2, double r3)
+    public static double calcArea(double r1, double r2, double r3)
     {
         double area; // of a filled area
         area = r3 * r3 * (2 - Math.PI / 4) + Math.PI * 3 / 8 * (r2 * r2 - r1 * r1);
