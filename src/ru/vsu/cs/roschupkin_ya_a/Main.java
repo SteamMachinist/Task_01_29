b0d38682f5186315077391d8e4dc1325c1f54b9c
Yaroslav Roschupkin <yaroslav-r.01@mail.ru>
2020-09-25 23:14:28 +0300

    -

--- src/ru/vsu/cs/roschupkin_ya_a/Main.java
+++ src/ru/vsu/cs/roschupkin_ya_a/Main.java
@@ -9,25 +9,32 @@ public class Main
     {
         Locale.setDefault(Locale.ROOT);
 
-        Scanner sc = new Scanner(System.in);
-        //entering radii of circles where r1 is the smallest
+        double radius1 = handleIn("smallest");
+        double radius2 = handleIn("medium");
+        double radius3 = handleIn("biggest");
 
-        System.out.print("enter the radius of the smallest circle R = ");
-        double r1 = sc.nextDouble();
-
-        System.out.print("enter the radius of the medium circle   R = ");
-        double r2 = sc.nextDouble();
-
-        System.out.print("enter the radius of the biggest circle  R = ");
-        double r3 = sc.nextDouble();
-
-        if (!(checkMistakes(r1, r2, r3))) System.out.printf("filled area is %.3f", calcArea(r1, r2, r3));
-
+        handleOut(radius1, radius2, radius3);
     }
 
-    public static boolean checkMistakes (double r1, double r2, double r3) //checking r3 > r2 > r1 > 0
+    static double handleIn (String radiusName)
     {
-        if (((r1 >= r2) || (r1 >= r3) || (r2 >= r3)) || (r1 < 0))
+        System.out.printf("enter the radius of the %s circle R = ", radiusName);
+        Scanner sc = new Scanner(System.in);
+        return sc.nextDouble();
+    }
+
+    static void handleOut (double radius1,double radius2, double radius3)
+    {
+        if (!hasError(radius1, radius2, radius3))
+        {
+            System.out.printf("filled area is %.3f", calcArea(radius1, radius2, radius3));
+        }
+    }
+
+
+    static boolean hasError (double radius1, double radius2, double radius3) //checking 0 < r1 < r2 < r3
+    {
+        if ((radius1 >= radius2) || (radius2 >= radius3) || (radius1 >= radius3) || (radius1 < 0))
         {
             System.out.println("something is wrong");
             return true;
@@ -35,18 +42,50 @@ public class Main
         return false;
     }
 
-    public static double calcArea(double r1, double r2, double r3)
+    static double calcArea(double radius1, double radius2, double radius3)
     {
         /*
            area = (1/4 of square area) + (1/4 of (square area - big circle area)) +
                 + (3/8 of (medium circle area - small circle area)) =
 
-                = 1/4 * (2*r3 * 2*r3) + 1/4 * (2*r3 * 2r3 - PI * r3 *r3) +
-                + 3/8 * (PI * r2 * r2 - PI * r1 * r1) =
-
-                = r3 * r3 * (2 - PI / 4) + PI * 3/8 * (r2 * r2 - r1 * r1)
+                = A1 + A2 + A3
          */
 
-        return r3 * r3 * (2 - Math.PI / 4) + Math.PI * 3 / 8 * (r2 * r2 - r1 * r1);
+        return calcA1(2 * radius3) + calcA2(radius3) + calcA3(radius1, radius2);
+    }
+
+    static double calcA1 (double squareSide) // 1/4 of square area
+    {
+        return 0.25 * calcSquareArea(squareSide);
+    }
+
+    static double calcSquareArea (double side)
+    {
+        return side * side;
+    }
+
+    static double calcA2 (double radius3) // 1/4 of (square - big circle) area
+    {
+        return 0.25 * (calcSquareArea(2 * radius3) - calcBigCircleArea(radius3));
+    }
+
+    static double calcBigCircleArea (double radius3)
+    {
+        return Math.PI * radius3 * radius3;
+    }
+
+    static double calcA3 (double radius1, double radius2) // 3/8 of (medium circle - small circle) area
+    {
+        return 0.375 * (calcMediumCircleArea(radius2) - calcSmallCircleArea(radius1));
+    }
+
+    static double calcMediumCircleArea(double radius2)
+    {
+        return Math.PI * radius2 * radius2;
+    }
+
+    static double calcSmallCircleArea (double radius1)
+    {
+        return Math.PI * radius1 * radius1;
     }
 }
